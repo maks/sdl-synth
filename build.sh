94f34bd0d8ebf97b3bd3b1ed0c2b1e5d0df8f556
Maksim Lin <maks@manichord.com>
2024-03-09 22:57:51 +1100

    minimal sdl audio lib usage

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cc `pkg-config sdl2 -cflags` synth.cpp
+export CFLAGS="-Wall -Werror `pkg-config sdl2 -cflags` "
+export LIBS=`pkg-config sdl2 -libs`
+
+cc $CFLAGS -o sdlsynth synth.cpp $LIBS
--- synth.cpp
+++ synth.cpp
@@ -1,7 +1,18 @@
 #include <cstdio>
 
+#include <SDL2/SDL.h>
+#include "SDL2/SDL_audio.h"
+
 int main(int argc, char const *argv[])
 {
   printf("sdl synth\n");
+
+  if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS) < 0) {
+    printf("Failed to initialize SDL: %s\n", SDL_GetError());
+    return 1;
+  }
+
+
+
   return 0;
 }
