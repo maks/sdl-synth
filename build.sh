9808a55c56e3df47b5938c11da1a20fb411efffa
Maksim Lin <maks@manichord.com>
2024-03-09 23:07:49 +1100

    basic audio callback

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-export CFLAGS="-Wall -Werror `pkg-config sdl2 -cflags` "
+set -xe 
+
+export CFLAGS="-Wall -Werror -std=c++20 -pedantic `pkg-config sdl2 -cflags` "
 export LIBS=`pkg-config sdl2 -libs`
 
 cc $CFLAGS -o sdlsynth synth.cpp $LIBS
--- synth.cpp
+++ synth.cpp
@@ -1,7 +1,26 @@
 #include <cstdio>
+#include <cassert>
 
 #include <SDL2/SDL.h>
 #include "SDL2/SDL_audio.h"
+
+
+const int SAMPLE_RATE = 44100;
+const int BUFFER_SIZE = 4096;
+
+
+// oscillator oscillate(float rate, float volume) {
+//   oscillator o = {
+//       .current_step = 0,
+//       .volume = volume,
+//       .step_size = (2 * M_PI) / rate,
+//   };
+//   return o;
+// }
+
+void oscillator_callback(void *userdata, Uint8 *stream, int len) {
+  assert(false);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -10,6 +29,31 @@ int main(int argc, char const *argv[])
   if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS) < 0) {
     printf("Failed to initialize SDL: %s\n", SDL_GetError());
     return 1;
+  }
+
+  SDL_AudioSpec spec = {
+      .freq = SAMPLE_RATE,
+      .format = AUDIO_U16,
+      .channels = 1,
+      .samples = 4096,
+      .callback = oscillator_callback,
+  };
+
+  if (SDL_OpenAudio(&spec, NULL) < 0) {
+    printf("Failed to open Audio Device: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_PauseAudio(0);
+
+  while (true) {
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+      switch (e.type) {
+      case SDL_QUIT:
+        return 0;
+      }
+    }
   }
 
 
