a1ed6f8a2004296d97dfe01512b5fb81ee11cf19
Maksim Lin <maks@manichord.com>
2024-03-18 20:09:36 +1100

    note pitch via sdl window key inputs
    bring back float use to get note pitch working

--- src/synth.cpp
+++ src/synth.cpp
@@ -8,6 +8,8 @@
 const int SAMPLE_RATE = 44100;
 const int BUFFER_SIZE = 256;
 
+static int note = 41; /* integer representing midi notes */
+
 //=== PICO SYNTH
 
 const int HARMONICS = 6;
@@ -34,39 +36,51 @@ Sint16 sine[LUT_SIZE] = {
  */
 int filt[HARMONICS] = {0, 0, 0, 0, 0, 0};
 u_char filt_state[HARMONICS] = {0, 0, 0, 0, 0, 0};
-int phase_int[HARMONICS] = {0, 0, 0, 0, 0, 0};
+float phase_int[HARMONICS] = {0, 0, 0, 0, 0, 0};
 
-int generatePhaseSample(int phase_increment, int &phase_int, int vol) {
+int generatePhaseSample(float phase_increment, float &phase_index, int vol) {
   int result = 0;
-  phase_int = phase_int;
-  phase_int += phase_increment;
+  phase_index = phase_index;
+  phase_index += phase_increment;
 
-  if (phase_int >= LUT_SIZE) {
-    int diff = phase_int - LUT_SIZE;
-    phase_int = diff;
+  if (phase_index >= LUT_SIZE) {
+    int diff = phase_index - LUT_SIZE;
+    phase_index = diff;
   }
 
-  result = sine[phase_int];
+  result = sine[(int)phase_index];
   int result_fp = i2fp(result);
   auto res_fp = fp_mul(result_fp, vol);
   return fp2i(res_fp);
 }
 
+/*
+  Calculate pitch from note value.
+  offset note by 57 halfnotes to get correct pitch from the range we have chosen
+  for the notes.
+*/
+static const float chromatic_ratio = 1.059463094359295264562;
+static float get_pitch(double note) {
+  double p = pow(chromatic_ratio, note - 57);
+  p *= 440;
+  return p;
+}
+
 void generateWaves(Uint8 *byte_stream) {
   Sint16 *s_byte_stream;
 
-  // auto phase_increment1 = ((float)440 / SAMPLE_RATE) * LUT_SIZE;
-  // auto phase_increment2 = ((float)880 / SAMPLE_RATE) * LUT_SIZE;
-  // auto phase_increment3 = ((float)1320 / SAMPLE_RATE) * LUT_SIZE;
-  // printf("INC1:%f INC2:%f\n INC3:%f\n", phase_increment1, phase_increment2,
-  //        phase_increment3);
-  // exit(1);
+  // get correct phase increment for note depending on sample rate and LUT
+  // length.
+  float baseNoteFreq = (get_pitch(note) / SAMPLE_RATE) * LUT_SIZE;
 
-  // Harmonics of primary freq, close to ordinals for a 100 sample sine LUT
-  int phase_increment[HARMONICS] = {1, 2, 3, 4, 5, 6};
+  // Harmonics of primary freq
+  float phase_increment[HARMONICS] = {
+      baseNoteFreq,     2 * baseNoteFreq, 3 * baseNoteFreq,
+      4 * baseNoteFreq, 5 * baseNoteFreq, 6 * baseNoteFreq,
+  };
 
   // max 42038 across all harmonics
-  int volume[HARMONICS] = {7000, 7000, 7000, 7000, 7000, 7000};
+  int volume[HARMONICS] = {3000, 3000, 3000, 3000, 3000, 3000};
 
   /* cast buffer as 16bit signed int */
   s_byte_stream = (Sint16 *)byte_stream;
@@ -88,15 +102,64 @@ void oscillator_callback(void *userdata, Uint8 *byteStream, int len) {
   generateWaves(byteStream);
 }
 
-int main(int argc, char const *argv[])
-{
+static void handle_note_keys(SDL_Keysym *keysym) {
+  /* change note or octave depending on which key is pressed */
+  int new_note = note;
+  switch (keysym->sym) {
+  case SDLK_a:
+    new_note = 34;
+    break;
+  case SDLK_s:
+    new_note = 37;
+    break;
+  case SDLK_d:
+    new_note = 41;
+    break;
+  case SDLK_f:
+    new_note = 49;
+    break;
+  }
+  printf("new note:%d\n", new_note);
+  note = new_note;
+}
+
+static void handle_key_down(SDL_Keysym *keysym) { handle_note_keys(keysym); }
+
+int main(int argc, char const *argv[]) {
   printf("sdl synth\n");
 
-  if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS) < 0)
-  {
+  if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_VIDEO) < 0) {
     printf("Failed to initialize SDL: %s\n", SDL_GetError());
     return 1;
   }
+
+  // SDL WINDOW setup
+
+  SDL_Window *window = NULL;
+  SDL_Renderer *renderer = NULL;
+  SDL_GLContext context;
+
+  window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                            640, 480, SDL_WINDOW_OPENGL);
+
+  if (window != NULL) {
+    context = SDL_GL_CreateContext(window);
+    if (context == NULL) {
+      printf("\nFailed to create context: %s\n", SDL_GetError());
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (renderer != NULL) {
+      SDL_GL_SetSwapInterval(1);
+      SDL_SetWindowTitle(window, "SDL2 synth sample 2");
+    } else {
+      printf("Failed to create renderer: %s", SDL_GetError());
+    }
+  } else {
+    printf("Failed to create window:%s", SDL_GetError());
+  }
+
+  // SDL window setup done ==========
 
   SDL_AudioSpec spec = {
       .freq = SAMPLE_RATE,
@@ -106,8 +169,7 @@ int main(int argc, char const *argv[])
       .callback = oscillator_callback,
   };
 
-  if (SDL_OpenAudio(&spec, NULL) < 0)
-  {
+  if (SDL_OpenAudio(&spec, NULL) < 0) {
     printf("Failed to open Audio Device: %s\n", SDL_GetError());
     return 1;
   } else {
@@ -116,13 +178,14 @@ int main(int argc, char const *argv[])
 
   SDL_PauseAudio(0);
 
-  while (true)
-  {
+  while (true) {
     SDL_Event e;
-    while (SDL_PollEvent(&e))
-    {
-      switch (e.type)
-      {
+    while (SDL_PollEvent(&e)) {
+      switch (e.type) {
+      case SDL_KEYDOWN:
+        handle_key_down(&e.key.keysym);
+        printf("NOTE FREQ:%f\n", get_pitch(note));
+        break;
       case SDL_QUIT:
         printf("exiting...\n");
         return 0;
